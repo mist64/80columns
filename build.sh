f649977f04a4b00f1ca55a00a339682eb0b300cf
Michael Steil <mist64@mac.com>
2017-07-10 21:28:36 +0200

    changed build script into a (naive) Makefile

--- build.sh
+++
@@ -1,7 +0,0 @@
-set -e
-ca65 80columns.s
-ca65 -o charset.o charset.s
-ld65 -C 80columns.cfg 80columns.o charset.o -o 80columns.bin
-echo -n -e '\x00\xc8' > 80columns.prg
-cat 80columns.bin >> 80columns.prg
-exomizer sfx 51200 -q -n -o 80columns-compressed.prg 80columns.prg
