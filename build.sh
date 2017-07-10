d183284e3a4ac34c26917298d254070e2f7563f0
Michael Steil <mist64@mac.com>
2017-07-10 21:23:32 +0200

    specified name of charset .o explicitly, so the charset source file can be changed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -e
 ca65 80columns.s
-ca65 charset.s
+ca65 -o charset.o charset.s
 ld65 -C 80columns.cfg 80columns.o charset.o -o 80columns.bin
 echo -n -e '\x00\xc8' > 80columns.prg
 cat 80columns.bin >> 80columns.prg
